2d946e88938a325c7c86bcf53aa1f009cc48d585
Prasanth Ganesan <erprasanthcs@gmail.com>
2025-12-15 21:41:25 +0530

    Implement isAnagram function to check anagrams

--- src/strings/ValidAnagram.ts
+++ src/strings/ValidAnagram.ts
@@ -1,1 +1,23 @@
+function isAnagram(s: string, t: string): boolean {
+  if(s.length != t.length){
+    return false
+  }
+  
+  let map = new Map<string,number>();
+  for(let charS of s){
+    let count = (map.get(charS) || 0)+1;
+    map.set(charS,count);
+  }
+    
+    for(let charT of t){
+        let count = map.get(charT);
 
+        if(!count){
+            return false;
+        }
+
+      map.set(charT,count-1);
+    }
+
+    return true;
+};
